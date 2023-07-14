4fa394f4b106ff1cff1f0a616830b631264f2a0c
AnnaFYZ <113105718+AnnaFYZ@users.noreply.github.com>
2023-07-14 23:01:00 +0100

    app created

--- src/App.js
+++ src/App.js
@@ -1,22 +1,66 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [quote, setQuote] = useState({"quote": "", "author": ""});
+  const [fetchValue, setFetchValue] = useState("quotes/random");
+
+  const myQuote = {"quote": "H E L l O O O O U!? The input is empty",
+"author": "Anna"}
+
+  useEffect(() => {fetch(`https://anna-quote-server.glitch.me/${fetchValue}`)
+  .then(res => res.json())
+  .then(data => {
+    setQuote(data);
+    setFetchValue("");})}, [fetchValue]) 
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="quotes">
+          {quote.length > 1 ? (
+            quote.map((quote) => (
+              <div>
+                <h3>{quote.quote}</h3>
+                <p>Author: {quote.author}</p>
+              </div>
+            ))
+          ) : (
+            <div>
+              <h3>{quote.quote}</h3>
+              <p>Author: {quote.author}</p>
+            </div>
+          )}
+        </div>
+        <div>
+          <input
+            type="text"
+            value={inputValue}
+            placeholder="Type a term you are looking for..."
+            onChange={(e) => setInputValue(e.target.value)}
+          ></input>
+          <div className="buttonsDiv">
+            <button
+              name="randomQuote"
+              className="buttons"
+              onClick={() => setFetchValue("quotes/random")}
+            >
+              Get random quote
+            </button>
+            <button
+              name="customQuote"
+              className="buttons"
+              onClick={() => {
+                inputValue === ""
+                  ? setQuote(myQuote)
+                  : setFetchValue(`quotes/search?term=${inputValue}`);
+              }}
+            >
+              Find the quote
+            </button>
+          </div>
+        </div>
       </header>
     </div>
   );
